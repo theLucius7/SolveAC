05a8ca6a2262c3e474fffcd7c6180322119e14c2
Lucius7nya <3012967200@qq.com>
2025-12-15 20:25:52 +0800

    Add template overrides for footer layout

---
+++ types/node.d.ts
@@ -0,0 +1,8 @@
+declare module 'node:path' {
+  export function resolve(...pathSegments: string[]): string;
+  export function dirname(p: string): string;
+}
+
+declare module 'node:url' {
+  export function fileURLToPath(url: string): string;
+}
